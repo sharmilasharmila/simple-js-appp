6f1f530b180e6148b4a08238bc92690360c0d086
sharmilasharmila <73405006+sharmilasharmila@users.noreply.github.com>
2020-11-24 11:10:27 +0100

    created array
    created array

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [];
+  pokemonList[0]={
+    name:'balbasour',
+    height:7,
+    type:['grass','poison']
+    };
+  pokemonList[1]={
+    name:'charmendor',
+    height:6,
+    type:['blaze','solar-power']
+    };
+  pokemonList[2]={
+    name:'squirtle',
+    height:5,
+    type:['Rain-dish','torrent']
+    };
